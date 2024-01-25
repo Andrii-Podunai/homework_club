7f59e6abdb68258a64aa8f996d6e2a83fe33e29a
Andrii Podunai <podunaiandriy2004@gmail.com>
2024-01-25 13:51:44 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log(typeof 12);
+console.log(typeof "12");
+console.log(typeof b);
+let a = null;
+console.log(typeof a);
+console.log(typeof 142353452n);
+console.log(typeof new Object());
+console.log(typeof true);
+
+let one = 15;
+let two = "15";
+console.log(one == two);
+console.log(one === two);
